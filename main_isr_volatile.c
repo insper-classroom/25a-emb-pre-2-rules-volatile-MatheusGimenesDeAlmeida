1e308ee7bb94d736fa5172036be0b01f4e83374a
MatheusGimenesDeAlmeida <mthga500@gmail.com>
2025-02-17 22:39:20 -0300

    Teste1

--- main_isr_volatile.c
+++ main_isr_volatile.c
@@ -1,16 +1,16 @@
 #include "asf.h"
 
 int g_cnt = 0;
-int f_btn = 0; // deveria ser volatile
+volatile int f_btn = 0; 
 
 // ISR
 void btn_callback(void) {
-  f_btn = 1; // variável alterada de uma interrupcão
+  f_btn = 1; 
 }
 
 void main(void) {
 
-  volatile int cnt = 0; // não precisa ser volatile
+  int cnt = 0; 
 
   while (1) {
     if (f_btn) {
